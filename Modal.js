61ed9f168eebe71ee9b362ccd73b631225f4ae33
Anthony Hein <anhein@princeton.edu>
2020-08-02 23:21:54 -0400

    Updated prices and CPM.
    CPM will only show for national advertisers.

--- Modal.js
+++ Modal.js
@@ -13,80 +13,75 @@ var btnClose = document.getElementById("closeModal");
 // Descriptions.
 
 var descrips = [
-    `Big, bold, and bound to be seen by the majority of The Daily Princetonian’s several hundred thousand online viewers, the Website Billboard ad covers the top of the website and will surely make a statement. 
-    <br><br>
-    CPM = $10.45
-    <br>
+    `
     Pixel Size: 970x270
     <br>
     For Higher Quality Images, Submit Double Resolution: 1940x540
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience.`,
-    `“Towering” over other ads, the Website Skyscraper ad allows you to clearly showcase your organization and reach our audience. 
-    <br><br>
-    CPM = $8.25
-    <br>
+    `,
+    `
     Pixel Size: 300x600
     <br>
     For Higher Quality Images, Submit Double Resolution: 600x1200
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience.`,
-    `A classic ad, the Website Banner ad is actually two ads - one banner ad in the middle of the website and one at the bottom - which means even more engagement. 
-    <br><br>
-    CPM = $5.95
-    <br>
+    `,
+    `
     Pixel Size: 728x90
     <br>
-    For Higher Quality Images, Submit Double Resolution: 1456x90
+    For Higher Quality Images, Submit Double Resolution: 1456x180
     <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience. 
+    The Website Banner includes two ad positions: one banner in the middle of the homepage and one at the bottom.  
     `, 
-    `One of our more affordable website ads but still impactful, the Website Block ad embodies “Great things come in small packages” and is a great option for first customers. 
-    <br>
-    <br>
-    CPM = $5.95
-    <br>
+    `
     Pixel Size: 300x250
     <br>
     For Higher Quality Images, Submit Double Resolution: 600x500
-    <br>
-    <br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience.`,
-    `With up to 55% open rates, more than 2 times the industry average, the breaking news email reservation is one of the best ways to engage with The Daily Princetonian’s subscribers. When you purchase a breaking news email (or emails), you reserve the next breaking news email(s) that comes out. 
     <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience.`,
-    `The first thing subscribers see upon opening our newsletter (every week day when school is in session; once a week when school is out of session), the Email Headline ad gives you direct access to potential customers. 
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience.`,
-    `More affordable than the Email Headline still effective, the Email In-Article ad is an effective alternative on our newsletter (every week day when school is in session; once a week when school is out of session).
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience.`,
-    `An easy way to directly reach out to the Prince community, the Facebook Post allows you to customize a powerful message to our audience. 
-    <br><br>
-    Please send us your ad and caption. 
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience. 
+    Note that this ad appears in the article on mobile (all readers must scroll past it) and next to the article on desktop. About 70% of our readers view our site on their mobile devices.
     `,
-    `The best way to connect with Princeton students, the Instagram Post is ideal for those looking to connect with the younger generations. 
-    <br><br>
-    Please send us your ad and caption. 
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience. 
+    `
+    With both click rates and open rates of more than 2 times the industry average, the breaking news email reservation is one of the best ways to engage with The Daily Princetonian’s subscribers. Our subscribers are students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics. A breaking news email reservation includes the next breaking news email(s) that comes out. 
     `,
-    `With some of the highest engagement of The Daily Princetonian’s products, the Twitter Post is a solid path to the Princeton community. 
-    <br><br>
-    Please send us your ad and caption. 
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience. 
+    `
+    With a 9.6% click rate, our readers engage with our newsletter at more than double the industry average. Email newsletters come out every weekday when school is in session and once per week when school is out of session. Our subscribers are mostly students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics.
     `,
-    `Played at the beginning of The Daily Princetonian’s widely watched media, the up-to-15-seconds Youtube Ad is the perfect medium for dynamic videos. 
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience. 
+    `
+    With a 9.6% click rate, our readers engage with our newsletter at more than double the industry average. Email newsletters come out every week day when school is in session and once per week when school is out of session. Our subscribers are mostly students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics.
     `,
-    `Read at the beginning of The Daily Princetonian’s podcasts (every week day when school is in session; once a week when school is out of session), the up-to-15-seconds Podcast ad allows you to directly voice your message. 
-    <br><br>
-    All digital advertisements can link to your website. More importantly, all digital advertisement purchases come with a complementary analytics report that tells you key statistics like how many impressions (views) you received and what percentage of viewers clicked on the ad. The Prince strives to offer the best experience. `
-]
+    `
+    With an average reach of 2000 users and a 10% average click rate, our sponsored Facebook posts offer a premium high-engagement opportunity. All we need from you is your ad, your link, and a caption.
+    `,
+    `
+    Our Instagram is a fantastic way to reach Princeton students and young alumni. All we need from you is your ad, your link, and a caption.
+    `,
+    `
+    Leverage our 15,000 Twitter followers. All we need from you is your ad, your link, and a caption.
+    `,
+    `
+    Played at the beginning of The Daily Princetonian’s widely watched media, your Youtube Ad can be up to 15 seconds long. It can be as simple as an image or a logo, or it can be a full-production video. 
+    `,
+    `
+    Read at the beginning of The Daily Princetonian’s podcasts (every week day when school is in session; once a week when school is out of session), your Podcast Ad can be up to 15 seconds long. 
+    `
+];
+
+var cpms = [
+    `CPM = $10.45<br>`,
+    `CPM = $8.25<br>`,
+    `CPM = $5.95<br>`,
+    `CPM = $5.95<br>`,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``,
+    ``
+];
 
 var headers = [
     `Website Billboard`,
@@ -101,30 +96,23 @@ var headers = [
     `Twitter Post`,
     `YouTube Ad`,
     `Podcast Ad`
-]
+];
 
-function modalFunc(k) {
-    console.log(k);
-    modal.className = "Modal is-visuallyHidden";
-    var descrip = document.getElementById("modal--descrip");
-    descrip.innerHTML = descrips[k];
-    var header = document.getElementById("modal--header");
-    header.innerHTML = headers[k];
-    setTimeout(function() {
-      body.className = "MainContainer is-blurred";
-      modal.className = "Modal";
-    }, 100);
-}
 
 var i = 0;
 for (i = 0; i < 12; i++) {
     var j
     for (j = 0; j < 3; j++) {
         const k = i;
+        const m = j;
         ratesCards[i + 17 * j].onclick = function () {
             modal.className = "Modal is-visuallyHidden";
             var descrip = document.getElementById("modal--descrip");
-            descrip.innerHTML = descrips[k];
+            if (m == 2) {
+                descrip.innerHTML = cpms[k] + descrips[k];
+            } else {
+                descrip.innerHTML = descrips[k];
+            }
             var header = document.getElementById("modal--header");
             header.innerHTML = headers[k];
             setTimeout(function() {

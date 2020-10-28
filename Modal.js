81cefa7bce58e5209cf928604589dbda365955e8
Anthony Hein <anhein@princeton.edu>
2020-10-28 00:20:23 -0400

    New rates.

--- Modal.js
+++ Modal.js
@@ -24,13 +24,6 @@ var descrips = [
     For Higher Quality Images, Submit Double Resolution: 600x1200
     `,
     `
-    Pixel Size: 728x90
-    <br>
-    For Higher Quality Images, Submit Double Resolution: 1456x180
-    <br><br>
-    The Website Banner includes two ad positions: one banner in the middle of the homepage and one at the bottom.  
-    `, 
-    `
     Pixel Size: 300x250
     <br>
     For Higher Quality Images, Submit Double Resolution: 600x500
@@ -38,13 +31,13 @@ var descrips = [
     Note that this ad appears in the article on mobile (all readers must scroll past it) and next to the article on desktop. About 70% of our readers view our site on their mobile devices.
     `,
     `
-    With both click rates and open rates of more than 2 times the industry average, the breaking news email reservation is one of the best ways to engage with The Daily Princetonian’s subscribers. Our subscribers are students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics. A breaking news email reservation includes the next breaking news email(s) that comes out. 
+    With both click rates and open rates of more than 2 times the industry average, the breaking news email reservation is one of the best ways to engage with The Daily Princetonian’s subscribers. Our subscribers are students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics. A breaking news email reservation includes the next breaking news email(s) that comes out.
     `,
     `
     With a 9.6% click rate, our readers engage with our newsletter at more than double the industry average. Email newsletters come out every weekday when school is in session and once per week when school is out of session. Our subscribers are mostly students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics.
     `,
     `
-    With a 9.6% click rate, our readers engage with our newsletter at more than double the industry average. Email newsletters come out every week day when school is in session and once per week when school is out of session. Our subscribers are mostly students, parents, faculty, alumni, and local Princeton residents; feel free to contact us for more details regarding our subscriber demographics.
+    With an average reach of 2000 users and a 10% average click rate, our sponsored Facebook posts offer a premium high-engagement opportunity. All we need from you is your ad, your link, and a caption.
     `,
     `
     With an average reach of 2000 users and a 10% average click rate, our sponsored Facebook posts offer a premium high-engagement opportunity. All we need from you is your ad, your link, and a caption.
@@ -53,13 +46,16 @@ var descrips = [
     Our Instagram is a fantastic way to reach Princeton students and young alumni. All we need from you is your ad, your link, and a caption.
     `,
     `
+    Our Instagram is a fantastic way to reach Princeton students and young alumni. All we need from you is your ad, your link, and a caption.
+    `,
+    `
     Leverage our 15,000 Twitter followers. All we need from you is your ad, your link, and a caption.
     `,
     `
-    Played at the beginning of The Daily Princetonian’s widely watched media, your Youtube Ad can be up to 15 seconds long. It can be as simple as an image or a logo, or it can be a full-production video. 
+    Played at the beginning of The Daily Princetonian’s widely watched media, your Youtube Ad can be up to 15 seconds long. It can be as simple as an image or a logo, or it can be a full-production video.
     `,
     `
-    Read at the beginning of The Daily Princetonian’s podcasts (every week day when school is in session; once a week when school is out of session), your Podcast Ad can be up to 15 seconds long. 
+    Read at the beginning of The Daily Princetonian’s podcasts (every week day when school is in session; once a week when school is out of session), your Podcast Ad can be up to 15 seconds long.
     `
 ];
 
@@ -67,7 +63,7 @@ var cpms = [
     `CPM = $10.45<br>`,
     `CPM = $8.25<br>`,
     `CPM = $5.95<br>`,
-    `CPM = $5.95<br>`,
+    ``,
     ``,
     ``,
     ``,
@@ -86,13 +82,13 @@ var cpms = [
 var headers = [
     `Website Billboard`,
     `Website Skyscraper`,
-    `Website Banner`,
     `Website Block`,
     `Breaking News Email Reservation`,
     `Email Headline`,
-    `Email In-Article`,
     `Facebook Post`,
+    `Facebook Story`,
     `Instagram Post`,
+    `Instagram Story`,
     `Twitter Post`,
     `YouTube Ad`,
     `Podcast Ad`

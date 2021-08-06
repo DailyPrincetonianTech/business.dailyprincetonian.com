68eea52b1828cb8825b6e2dc3b809ec88ab57e5f
Pranav Avva <pranav.avva@gmail.com>
2021-08-06 18:50:39 -0400

    move images to s3

--- FallIssue.js
+++ FallIssue.js
@@ -49,19 +49,19 @@ fallBtnCampus.onclick = function() {
     <div class="fall--options--row">
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Full.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/full.png">
                 <p>Full Page<br>$960</p>
             </div>
         </div>
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Half.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/half.png">
                 <p>Half Page*<br>$580</p>
             </div>
         </div>
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Quarter.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/quarter.png">
                 <p>Quarter Page<br>$340</p>
             </div>
         </div>
@@ -69,14 +69,14 @@ fallBtnCampus.onclick = function() {
     <div class="fall--options--row">
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Eighth.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/eighth.png">
                 <p>Eighth Page*<br>$190</p>
             </div>
         </div>
         <div class="fall--option fall--option--truck" style="margin: 0;">
             <div class="fall--option--container">
-                <img src="FallPrint/Truck1.png">
-                <img src="FallPrint/Truck2.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/truck-1.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/truck-2.png">
                 <p>Double Truck**<br>Reserved for<br>Lead Advertiser</p>
             </div>
         </div>
@@ -135,19 +135,19 @@ fallBtnGeneral.onclick = function() {
     <div class="fall--options--row">
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Full.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/full.png">
                 <p>Full Page<br>$1580</p>
             </div>
         </div>
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Half.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/half.png">
                 <p>Half Page*<br>$940</p>
             </div>
         </div>
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Quarter.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/quarter.png">
                 <p>Quarter Page<br>$560</p>
             </div>
         </div>
@@ -155,14 +155,14 @@ fallBtnGeneral.onclick = function() {
     <div class="fall--options--row">
         <div class="fall--option">
             <div class="fall--option--container">
-                <img src="FallPrint/Eighth.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/eighth.png">
                 <p>Eighth Page*<br>$310</p>
             </div>
         </div>
         <div class="fall--option fall--option--truck" style="margin: 0;">
             <div class="fall--option--container">
-                <img src="FallPrint/Truck1.png">
-                <img src="FallPrint/Truck2.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/truck-1.png">
+                <img src="https://assets.dailyprincetonian.com/business.dailyprincetonian.com/fall-print/truck-2.png">
                 <p>Double Truck**<br>Reserved for<br>Lead Advertiser</p>
             </div>
         </div>

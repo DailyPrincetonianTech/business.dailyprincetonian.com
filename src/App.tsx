506376e2a028d40996215ef27d146a2a8f839f7b
echoi10 <86752841+echoi10@users.noreply.github.com>
2021-11-14 22:20:19 -0500

    added for loop

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,77 @@ import React from "react";
 // import { store } from "./state/store";
 
 import "./App.css";
+import campusAdvertiser from "./data/campus";
+import localAdvertiser from "./data/local";
+import nationalAdvertiser from "./data/national";
+import recruiterAdvertiser from "./data/recruiter";
+import { AdGroup } from "./types/adGroup";
+import { Advertiser } from "./types/advertiser";
+import { Rate } from "./types/rate";
+import { Unit } from "./types/unit";
 
-function App(): JSX.Element {
+export default function App() {
 
   // return <Provider store={store}>Hello world!</Provider>;
   return (
-    <>Hello world!</>
+    <div>
+      {campusAdvertiser.map((adGroup: AdGroup, i) => (
+        <div>
+          {adGroup.title} {adGroup.rates.map((rate: Rate, i) => (
+            <div>
+              {rate.title} {rate.units.map((unit: Unit, i) => (
+                <div>{unit.title} {unit.price}</div>
+              ))}
+            </div>
+          ))}
+          <br></br>
+        </div>
+      ))}
+      <br></br>
+      <div>
+      {localAdvertiser.map((adGroup: AdGroup, i) => (
+        <div>
+          {adGroup.title} {adGroup.rates.map((rate: Rate, i) => (
+            <div>
+              {rate.title} {rate.units.map((unit: Unit, i) => (
+                <div>{unit.title} {unit.price}</div>
+              ))}
+            </div>
+          ))}
+          <br></br>
+        </div>
+      ))}
+      </div>
+      <br></br>
+      <div>
+      {nationalAdvertiser.map((adGroup: AdGroup, i) => (
+        <div>
+          {adGroup.title} {adGroup.rates.map((rate: Rate, i) => (
+            <div>
+              {rate.title} {rate.units.map((unit: Unit, i) => (
+                <div>{unit.title} {unit.price}</div>
+              ))}
+            </div>
+          ))}
+          <br></br>
+        </div>
+      ))}
+      </div>
+      <br></br>
+      <div>
+      {recruiterAdvertiser.map((adGroup: AdGroup, i) => (
+        <div>
+          {adGroup.title} {adGroup.rates.map((rate: Rate, i) => (
+            <div>
+              {rate.title} {rate.units.map((unit: Unit, i) => (
+                <div>{unit.title} {unit.price}</div>
+              ))}
+            </div>
+          ))}
+          <br></br>
+        </div>
+      ))}
+      </div>
+    </div>
   );
 }
-
-export default App;

27850e375f1c36285095bf3c09c9db71a781f043
Tai Nguyen <taisnguyen123@yahoo.com>
2022-11-16 16:30:30 -0500

    App defaultly runs in development mode.

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ the application code.
 '''
 
 from app import init_app
-from config import Config
+from config import DevelopmentConfig
 
 # Initialize and run a new app instance. 
-app = init_app(Config)
+app = init_app(DevelopmentConfig)
 if __name__ == "__main__":
     app.run()

d3efa90ce920fbce8866285bfb278a3fc628b809
Tai Nguyen <taisnguyen123@yahoo.com>
2022-11-17 19:08:42 -0500

    Added imports to additional models.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,7 @@
 '''Imports all submodules of models package'''
 
 from app.models import advertisement
+from app.models import advertisement_asterisk
+from app.models import advertisement_option
+from app.models import advertisement_popup
 from app.models import audience

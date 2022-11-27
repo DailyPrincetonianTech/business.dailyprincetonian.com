e69dc994365a3bb2e01a85fb646754ed95563905
Tai Nguyen <taisnguyen123@yahoo.com>
2022-11-27 00:31:41 -0500

    Updated Advertisement model to include title as property.

--- app/models/advertisement/__init__.py
+++ app/models/advertisement/__init__.py
@@ -1,12 +1,13 @@
 '''Defines the Advertisement model and its schema.'''
 
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 import sqlalchemy as sa
 from app.database import db
 
 
 class Advertisement(db.Model):
     id          = sa.Column(sa.Integer, primary_key = True)
+    title       = sa.Column(sa.String, nullable = False)
     audience_id = sa.Column(sa.Integer, sa.ForeignKey("audience.id"), nullable = False)
     clickable   = sa.Column(sa.Boolean, nullable = False)
     
@@ -16,6 +17,7 @@ class _AdvertisementSchema(SQLAlchemyAutoSchema):
         model = Advertisement
         load_instance = True
         include_fk = True
-
+        include_relationships = True
+        
 # Singleton instance of schema to be imported.
 advertisement_schema = _AdvertisementSchema()

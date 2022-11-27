35483941b1f2131829c10a1ee468d0e12e2cf9f3
Tai Nguyen <taisnguyen123@yahoo.com>
2022-11-27 00:30:49 -0500

    Added db data seeding.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from app.database import db
 
+from app.models.audience import Audience
+
 # View imports.
 from app.views import home 
+from app.views import upload
 
 
 def init_app(config_object):
@@ -16,7 +19,28 @@ def init_app(config_object):
         db.init_app(app)
         db.create_all()
         
+        # Seeded data.
+        _seed_data()
+        
         # Register blueprints.
-        app.register_blueprint(home.views.blueprint)        
+        app.register_blueprint(home.views.blueprint)
+        app.register_blueprint(upload.views.blueprint)
         
         return app
+    
+
+def _seed_data():
+    # Seeded audience data.
+    audiences = []
+    audiences.append(Audience(id = 0, name = "Princeton Student or Organization"))
+    audiences.append(Audience(id = 1, name = "Local Advertiser (Mercer County)"))
+    audiences.append(Audience(id = 2, name = "National Advertiser"))
+    audiences.append(Audience(id = 3, name = "Recruiter"))
+    
+    # Check if audience data already exists.
+    for audience in audiences:
+        if not db.session.query(Audience).filter_by(name = audience.name).first():
+            db.session.add(audience)
+    
+    # Commit to database.
+    db.session.commit()

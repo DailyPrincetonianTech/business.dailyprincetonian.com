e9d8cb4a7bda92fe3ebfbe62396f995f2da934ae
Tai Nguyen <taisnguyen123@yahoo.com>
2022-11-30 02:52:00 -0500

    Added marshmallow relationships.

--- app/models/advertisement/__init__.py
+++ app/models/advertisement/__init__.py
@@ -1,7 +1,11 @@
 '''Defines the Advertisement model and its schema.'''
 
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, fields
 from app.database import db
+
+from app.models.advertisement_option import AdvertisementOptionSchema
+from app.models.advertisement_asterisk import AdvertisementAsteriskSchema
+from app.models.advertisement_popup import AdvertisementPopupSchema
 
 
 class Advertisement(db.Model):
@@ -10,9 +14,18 @@ class Advertisement(db.Model):
     audience_id = db.Column(db.Integer, db.ForeignKey("audience.id"), nullable = False)
     image_url   = db.Column(db.String, nullable = False)
     clickable   = db.Column(db.Boolean, nullable = False)
+    # Constituent models.
+    options     = db.relationship("AdvertisementOption", backref = "advertisement", lazy = "joined")
+    asterisks   = db.relationship("AdvertisementAsterisk", backref = "advertisement", lazy = "joined")
+    popup       = db.relationship("AdvertisementPopup", backref = "advertisement", lazy = "joined", uselist = False)
     
     
-class _AdvertisementSchema(SQLAlchemyAutoSchema):
+class AdvertisementSchema(SQLAlchemyAutoSchema):
+    # Constituent models.
+    options   = fields.Nested(AdvertisementOptionSchema(only = ["label"]), many = True)
+    asterisks = fields.Nested(AdvertisementAsteriskSchema(only = ["label"]), many = True)
+    popup     = fields.Nested(AdvertisementPopupSchema(only = ["description"]), many = False)
+    
     class Meta:
         model = Advertisement
         load_instance = True
@@ -20,4 +33,4 @@ class _AdvertisementSchema(SQLAlchemyAutoSchema):
         include_relationships = True
         
 # Singleton instance of schema to be imported.
-advertisement_schema = _AdvertisementSchema()
+advertisement_schema = AdvertisementSchema()

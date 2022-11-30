3c846d9092b26ff1f1aa2ab4b51f6b6869a96509
Tai Nguyen <taisnguyen123@yahoo.com>
2022-11-30 02:51:41 -0500

    Changed import order.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,11 @@
 '''Imports all submodules of models package'''
 
-from app.models import advertisement
 from app.models import advertisement_asterisk
 from app.models import advertisement_option
 from app.models import advertisement_popup
+
+# These imports must be at the bottom to have access
+# to the above constituent models. Audience must be
+# below advertisement so it has access to it.
+from app.models import advertisement 
 from app.models import audience

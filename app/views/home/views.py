be4f4343cc0810405864cab840271744dbec46c0
Tai Nguyen <taisnguyen123@yahoo.com>
2022-12-05 09:13:46 -0500

    Added Jinja populating view with advertisement and audience data.

--- app/views/home/views.py
+++ app/views/home/views.py
@@ -1,11 +1,24 @@
 from flask import Blueprint, render_template
-from app.database import db
+from app.models.audience import Audience
+from app.services.advertisement import get_all_advertisements_by_audience_id
 
+from app.models.audience import audience_schema
 
 blueprint = Blueprint("home", __name__)
+
 
 # GET /
 @blueprint.route("/")
 def index():
+    advertisements = []
+    for audience_id in range(Audience.query.count()):
+        advertisements.append(get_all_advertisements_by_audience_id(audience_id))
 
-    return render_template("home/index.html")
+    audiences = []
+    for audience in Audience.query.all():
+        audiences.append(audience_schema.dump(audience))
+        
+
+    print(advertisements)
+
+    return render_template("home/index.html", audiences = audiences, advertisements = advertisements)

f40cd5390270c4d47f58b5f6d91c8159229da42a
Tai Sanh Nguyen <tai.sanh.ng@gmail.com>
2023-01-08 03:18:59 -0500

    Improved file processing times.

--- app/services/advertisement/__init__.py
+++ app/services/advertisement/__init__.py
@@ -1,8 +1,11 @@
 '''Advertisement repository to encapsulate database operations.'''
 
+from typing import List
+
 from attrs import define
 
 from app.database import db
+from sqlalchemy import delete
 from sqlalchemy.exc import IntegrityError
 from app.models.advertisement import Advertisement
 from app.models.advertisement_option import AdvertisementOption
@@ -110,169 +113,131 @@ def get_all_advertisements_by_audience_id(audience_id):
 
 
 
-def create_advertisement(dto: AdvertisementObjectOptions):
+def create_advertisements(dtos: List[AdvertisementObjectOptions]):
     '''
-    Creates an advertisement in the database,
+    Creates advertisements in the database,
     while also creating the appropriate constituent
     objects, such as AdvertisementOption, AdvertisementAsterisk,
     AdvertisementPopup objects, in the database as well.
     
     @return: errors, where errors is a list of errors that occurred during
-    the creation process. If errors is empty, then the advertisement has been
+    the creation process. If errors is empty, then the advertisements have been
     created successfully.
     '''
-    
+
     # Is returned to the caller and includes any errors that has occurred.
     # If errors is empty, then the advertisement has been created successfully.
     errors = []
     
-    
-    # Check if passed-in DTO is valid.
-    if (dto is None):
-        errors.append("No DTO was provided.")
-        return errors
+
+    last_advertisement = db.session.query(Advertisement).order_by(Advertisement.id.desc()).first()
+    starting_advertisement_id = (last_advertisement.id + 1) if last_advertisement is not None else 1
+
+    # Create advertisements.
     try:
-        dto.validate()
-    except Exception as e:
-        errors.append("Invalid DTO passed in:", e)
-        return errors
+        advertisement_mappings = []
+        current_advertisement_id = starting_advertisement_id
+        for dto in dtos:
+            advertisement_mappings.append({
+                "id": current_advertisement_id,
+                "title": dto.title,
+                "audience_id": dto.audience_id,
+                "image_url": dto.image_url,
+                "clickable": dto.popup is not None
+            })
+            current_advertisement_id += 1
 
-    # Check if advertisement with the same title/audience_id 
-    # combination already exists.
-    if (Advertisement.query.filter_by(title=dto.title, audience_id=dto.audience_id).first() is not None):
-        errors.append("An advertisement with the same title and audience ID already exists." + dto.title)
-        return errors
-
-
-    # Create the advertisement.
-    try:
-        advertisement = Advertisement(
-            title = dto.title,
-            audience_id = dto.audience_id,
-            image_url = dto.image_url,
-            clickable = dto.popup is not None
+        db.session.bulk_insert_mappings(
+            Advertisement, 
+            advertisement_mappings
         )
-        
-        db.session.add(advertisement)
         db.session.flush()
-    except IntegrityError:
+
+    except IntegrityError as e:
         errors.append("Audience ID does not exist: " + str(dto.audience_id))
         db.session.rollback()
         return errors
     except Exception as e:
-        errors.append(str(e))
+        errors.append("Error creating advertisement: " + str(e))
         db.session.rollback()
 
 
     # Create advertisement options.
     try:
-        for option_label, cost in zip(dto.option_labels, dto.costs):
-            advertisement_option = AdvertisementOption(
-                advertisement_id = advertisement.id,
-                label = option_label,
-                cost  = cost
-            )
-            db.session.add(advertisement_option)
-            db.session.flush()
-    except Exception as e:
-        errors.append(str(e))
-        db.session.rollback()
-    
-    # Create asterisks.
-    try:
-        if dto.asterisks:
-            for asterisk in dto.asterisks:
-                advertisement_asterisk = AdvertisementAsterisk(
-                    advertisement_id = advertisement.id,
-                    label = asterisk
-                )
-                db.session.add(advertisement_asterisk)
-                db.session.flush()
-    except Exception as e:
-        errors.append(str(e))
-        db.session.rollback()
-            
-    # Create popup.
-    try:
-        if dto.popup:
-            advertisement_popup = AdvertisementPopup(
-                advertisement_id = advertisement.id,
-                description = dto.popup
-            )
-            db.session.add(advertisement_popup)
-            db.session.flush()
-    except Exception as e:
-        errors.append(str(e))
-        db.session.rollback()
+        advertisement_option_mappings = []
+        current_advertisement_id = starting_advertisement_id
+        for dto in dtos:
+            for option_label, cost in zip(dto.option_labels, dto.costs):
+                advertisement_option_mappings.append({
+                    "advertisement_id": current_advertisement_id,
+                    "label": option_label,
+                    "cost": cost
+                })
+            current_advertisement_id += 1
 
-    # We are rollbacking here regardless of whether there
-    # are any objects in the session if errors exist.
-    if errors:
-        db.session.rollback()
-    
-    # Will either have errors or be empty.
-    return errors
-
-
-
-def delete_advertisement_by_id(advertisement_id):
-    # Is returned to the caller and includes any errors that has occurred.
-    # If errors is empty, then the advertisement has been created successfully.
-    errors = []
-    
-
-    # Check if advertisement exists.
-    advertisement = Advertisement.query.filter_by(id = advertisement_id).first()
-    if advertisement is None:
-        errors.append("No advertisement with the given title and audience ID exists.")
-        return errors
-    
-    
-    # Delete constituent objects.
-    # Delete all existing options for the advertisement.
-    try:
-        preexisting_options = AdvertisementOption.query.filter_by(advertisement_id = advertisement_id).all() 
-        for preexisting_option in preexisting_options:
-            db.session.delete(preexisting_option)
-            db.session.flush()
-    except Exception as e:
-        errors.append(str(e))
-        db.session.rollback()
-    
-    # Delete all existing asterisks for the advertisement.
-    try:
-        preexisting_asterisks = AdvertisementAsterisk.query.filter_by(advertisement_id = advertisement_id).all() 
-        for preexisting_asterisk in preexisting_asterisks:
-            db.session.delete(preexisting_asterisk)
-            db.session.flush()
-    except Exception as e:
-        errors.append(str(e))
-        db.session.rollback()
-        
-    # Delete existing popup for the advertisement.
-    try:
-        hasPopup = False
-        preexisting_popup = AdvertisementPopup.query.filter_by(advertisement_id = advertisement_id).first()
-        if preexisting_popup:
-            db.session.delete(preexisting_popup)
-            db.session.flush()
-    except Exception as e:
-        errors.append(str(e))
-        db.session.rollback()
-        
-    # Delete the advertisement itself.
-    try:
-        db.session.delete(advertisement)
+        db.session.bulk_insert_mappings(
+            AdvertisementOption, 
+            advertisement_option_mappings
+        )
         db.session.flush()
+
     except Exception as e:
-        errors.append(str(e))
+        errors.append("Error creating advertisement options: " + str(e))
         db.session.rollback()
+
+
+    # Create advertisement asterisks.
+    try:
+        advertisement_asterisk_mappings = []
+        current_advertisement_id = starting_advertisement_id
+        for dto in dtos:
+            if dto.asterisks:
+                for asterisk in dto.asterisks:
+                    advertisement_asterisk_mappings.append({
+                        "advertisement_id": current_advertisement_id,
+                        "label": asterisk
+                    })
+            current_advertisement_id += 1
+
+        db.session.bulk_insert_mappings(
+            AdvertisementAsterisk, 
+            advertisement_asterisk_mappings
+        )
+        db.session.flush()
+
+    except Exception as e:
+        errors.append("Error creating advertisement asterisks: " + str(e))
+        db.session.rollback()
+
+
+    # Create advertisement popups.
+    try:
+        advertisement_popup_mappings = []
+        current_advertisement_id = starting_advertisement_id
+        for dto in dtos:
+            if dto.popup:
+                advertisement_popup_mappings.append({
+                    "advertisement_id": current_advertisement_id,
+                    "description": dto.popup
+                })
+            current_advertisement_id += 1
+
+        db.session.bulk_insert_mappings(
+            AdvertisementPopup, 
+            advertisement_popup_mappings
+        )
+        db.session.flush()
     
+    except Exception as e:
+        errors.append("Error creating advertisement popups: " + str(e))
+        db.session.rollback()
+
+
     # We are rollbacking here regardless of whether there
     # are any objects in the session if errors exist.
     if errors:
         db.session.rollback()
-
+    
     # Will either have errors or be empty.
     return errors
 
@@ -280,27 +245,33 @@ def delete_advertisement_by_id(advertisement_id):
 
 def delete_all_advertisements():
     # Is returned to the caller and includes any errors that has occurred.
-    # If errors is empty, then the advertisement has been created successfully.
+    # If errors is empty, then all advertisements have been deleted successfully.
     errors = []
-    
-    
-    # Get all advertisement ids.
-    advertisement_ids = [advertisement.id for advertisement in Advertisement.query.all()]
-    
-    # Delete each advertisement.
-    for advertisement_id in advertisement_ids:
-        errors = delete_advertisement_by_id(advertisement_id)
-    
-    # Restart the auto-incrementing id.
+
+
     try:
-        db.session.execute("ALTER SEQUENCE advertisement_id_seq RESTART")
-        db.session.execute("ALTER SEQUENCE advertisement_option_id_seq RESTART")
-        db.session.execute("ALTER SEQUENCE advertisement_asterisk_id_seq RESTART")
-        db.session.execute("ALTER SEQUENCE advertisement_popup_id_seq RESTART")
-        db.session.flush()
+        delete_statements = []
+
+        # Delete all advertisement options.
+        delete_statements.append(delete(AdvertisementOption))
+
+        # Delete all advertisement asterisks.
+        delete_statements.append(delete(AdvertisementAsterisk))
+
+        # Delete all advertisement popups.
+        delete_statements.append(delete(AdvertisementPopup))
+
+        # Delete all advertisements.
+        delete_statements.append(delete(Advertisement))
+
+        # Execute all delete statements.
+        for delete_statement in delete_statements:
+            db.session.execute(delete_statement)
+
     except Exception as e:
-        errors.append(str(e) + ". Contact the administrator.")
+        errors.append("Error deleting all advertisements: " + str(e))
         db.session.rollback()
+
     
     # We are rollbacking here regardless of whether there
     # are any objects in the session if errors exist.
